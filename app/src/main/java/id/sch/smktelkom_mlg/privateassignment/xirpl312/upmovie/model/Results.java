fd2f74a4c3ae98de72069fab41153bbefb10cd04
Agnes-PC <fitria.mela9a17@gmail.com>
2017-05-13 19:10:17 +0700

    awal, dan penambahan model

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl312/upmovie/model/Results.java
@@ -0,0 +1,12 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl312.upmovie.model;
+
+import java.util.List;
+
+/**
+ * Created by Agnes-PC on 13/05/2017.
+ */
+
+public class Results {
+    public String page;
+    public List<Results> results;
+}
